4d62232446e09e06166c85ef85685c5b3def2e9b
jonrob <jonrob08@gmail.com>
2023-02-02 16:46:58 -0700

    chore: building out left sidebar

--- birds-i-vue/app.vue
+++ birds-i-vue/app.vue
@@ -1,27 +1,26 @@
 <template>
-  <div :class="{'dark': darkMode}">
-    
-    <div class="min-h-full">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-        <!--left sidebar-->
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!--left sidebar-->
 
-        <div class=" hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
-          asd
-        </div>
 
-        <!--main content-->
-        <!--right sidebar-->
+          <!--main content-->
+          <!--right sidebar-->
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 <script setup>
 
-    const darkMode = ref(true);
+const darkMode = ref(false);
 
 </script>
 
