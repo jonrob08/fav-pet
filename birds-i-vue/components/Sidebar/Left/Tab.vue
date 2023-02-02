b2b10c7590fd553ea77e941c109dec2fbe101304
jonrob <jonrob08@gmail.com>
2023-02-02 16:57:24 -0700

    chore: working on sidebar tab

---
+++ birds-i-vue/components/Sidebar/Left/Tab.vue
@@ -0,0 +1,8 @@
+<template>
+    <nuxt-link to="#">
+        <slot></slot>
+    </nuxt-link>
+</template>
+<script setup>
+
+</script>
---
+++ birds-i-vue/components/Sidebar/Left/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <div class="p-2 my-2 hover:bg-blue-500 rounded-full w-min dark:hover:bg-white/20" :class="defaultTransition">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+
+        <SidebarLeftTab>
+            <HomeIcon />
+        </SidebarLeftTab>
+
+    </div>
+</template>
+<script setup>
+import { HomeIcon } from "@heroicons/vue/solid"
+const { defaultTransition } = useTailwindConfig()
+</script>
