f23854779c76a464441ffbb184aec6766db5783b
jonrob <jonrob08@gmail.com>
2023-02-02 16:11:23 -0700

    chore: darkmode setup

--- birds-i-vue/app.vue
+++ birds-i-vue/app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <h1 class="text-red-500">Birds i Vue</h1>
+  <div :class="{'dark': darkMode}">
+    <h1 class="text-red-500 dark:text-dim-800">Birds i Vue</h1>
   </div>
 </template>
+<script setup>
+
+    const darkMode = ref(true);
+
+</script>
+
