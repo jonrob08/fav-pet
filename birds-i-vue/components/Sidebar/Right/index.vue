9a9a29508c546544fe3dd242567883f67b24a63b
jonrob <jonrob08@gmail.com>
2023-02-03 11:23:44 -0700

    chore: add data to right sidebar card

--- birds-i-vue/components/Sidebar/Right/index.vue
+++ birds-i-vue/components/Sidebar/Right/index.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="flex flex-col">
         <!-- Preview Card: What's Happening? -->
-        <SidebarRightPreviewCard title="What's Happening?" />
+        <SidebarRightPreviewCard title="What's Happening?" >
+            <SidebarRightPreviewCardItem v-for="whatsHappening in whatsHappeningItems">
+                <h2>{{ whatsHappening.title }}</h2>
+                <p>{{ whatsHappening.count }}</p>
+            </SidebarRightPreviewCardItem>
+
+        </SidebarRightPreviewCard>
         <!-- Preview Card: Who to Follow -->
-        <SidebarRightPreviewCard title="Who to Follow" />
+        <SidebarRightPreviewCard title="Who to Follow" >
+            <SidebarRightPreviewCardItem></SidebarRightPreviewCardItem>
+
+        </SidebarRightPreviewCard>
     </div>
 </template>
 <script setup>
+const whatsHappeningItems = ref([
+    {
+        title: 'SpaceX',
+        count: '18.8k Tweets'
+    },
+    {
+        title: '#VueIsFun',
+        count: '10.3k Tweets'
+    },
+    {
+        title: '#tailwindforall',
+        count: '20.4k Tweets'
+    }
+])
 
 </script>
