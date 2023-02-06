87c4fc665691d7dd7c540dc75b9d57a0634a5318
jonrob <jonrob08@gmail.com>
2023-02-06 10:13:12 -0700

    test: send multipart data
    good test in postman, going to continue building this out

--- fav-pet/server/api/auth/user/pets/index.post.js
+++ fav-pet/server/api/auth/user/pets/index.post.js
@@ -1,6 +1,19 @@
+import formidable from "formidable"
+
 export default defineEventHandler(async (event) => {
+    const form = formidable({})
+    const response = await new Promise((resolve, reject) => {
+        
+            form.parse(event.req, (err, fields, files) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve({fields, files})
+            })
+
+    })
 
     return {
-        hello: 'world'
+        hello: response
     }
 })
